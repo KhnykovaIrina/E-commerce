d23a6460355e61c13ccc990e4348de2f9460742b
KireikouEgor <99760603+KireikouEgor@users.noreply.github.com>
2022-02-23 21:40:56 +0300

    Created a component ShoppingCartButton

---
+++ src/components/ShoppingCartButton/ShoppingCartButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ShoppingCartButton.css';
+
+const ShoppingCartButton = () => (
+  <div className="ShoppingCartButton">    
+    ShoppingCartButton Component
+  </div>
+);
+
+export default ShoppingCartButton;
