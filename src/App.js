eba3e2bdc7f259b3ad8eb9ae107d0a45164719cb
KireikouEgor <99760603+KireikouEgor@users.noreply.github.com>
2022-02-24 12:18:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import ShoppingCartButton from './components/ShoppingCartButton';
 
+
 function App() {
   return (
     <div>
       <ShoppingCartButton text='Add to Cart - $250' />
-      <ShoppingCartButton text='Add to Cart - $250' disabled={true} />
-      <ShoppingCartButton text='Add to Cart - $250' />
+      <ShoppingCartButton text='Add to Cart - $250' disabled={true} /> 
     </div>
   );
 }
