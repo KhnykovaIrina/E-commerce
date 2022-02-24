0a0560d3692d28213edd41663f86f47df580052c
KireikouEgor <99760603+KireikouEgor@users.noreply.github.com>
2022-02-24 23:09:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import ShoppingCartButton from './components/ShoppingCartButton';
-
+import ShoppingCartButton from "./components/ShoppingCartButton";
+import React from "react";
 
 function App() {
-  return (
-    <div>
-      <ShoppingCartButton text='Add to Cart - $250' />
-      <ShoppingCartButton text='Add to Cart - $250' disabled={true} /> 
-    </div>
-  );
+    return (
+        <div>
+            <ShoppingCartButton text="Add to Cart - $250"/>
+            <ShoppingCartButton text="Add to Cart - $250" disabled={true}/>
+            <ShoppingCartButton backgroundColor={"#ebee0a"} text="Add to Cart - $250"/>
+        </div>
+    );
 }
 
 export default App;
