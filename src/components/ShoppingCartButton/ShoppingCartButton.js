5380b98a06eb63259da213549e158308f374de96
KireikouEgor <99760603+KireikouEgor@users.noreply.github.com>
2022-02-24 12:41:08 +0300

    Update ShoppingCartButton.js

--- src/components/ShoppingCartButton/ShoppingCartButton.js
+++ src/components/ShoppingCartButton/ShoppingCartButton.js
@@ -5,8 +5,8 @@ import { Button } from 'react-bootstrap';
 const ShoppingCartButton = (props) => {
   const text = props.text
   const disabled = props.disabled
-   
-  return <Button  variant='dark' className='rounded-0' disabled={disabled}>{text}</Button>
+     
+  return <Button variant='dark' className='rounded-0' disabled={disabled}>{text}</Button>
 };
 
 export default ShoppingCartButton;
