f64b7383df1420353c02e868b22c5ed91993b515
KireikouEgor <kireikou.js@gmail.com>
2022-02-27 16:50:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ShoppingCartButton from './components/ShoppingCartButton/ShoppingCartButton';
+import OutlineButton from './components/OutlineButton';
 
 function App() {
     return (
         <Container >
             <Row className="text-center">
                 <Col md={{ span: 6, offset: 3 }}>
-                    <h6 className="mt-3">Main - 2</h6>
+                    <h6 className="mt-3">Main - 6</h6>
                     <div>
-                        <ShoppingCartButton text="Add to Cart - $250" />
-                        <ShoppingCartButton text="Add to Cart - $250" disabled={true} />
-                        <ShoppingCartButton backgroundColor={"#005f73"} text="Add to Cart - $250" />
+                        <OutlineButton text="Shop All" />
+                        <OutlineButton text="Buy Now" />                       
                     </div>
                 </Col>
             </Row>
