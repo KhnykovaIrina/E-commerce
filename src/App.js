5350b3bd400bb2648ba85192b83802d86ba71f2d
KireikouEgor <kireikou.js@gmail.com>
2022-03-02 19:14:26 +0300

    Updated files App.js index.js  ProductSizes.js ProductSizes.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ShoppingCartButton from './components/ShoppingCartButton';
+import ProductSizes from "./components/ProductSizes";
+import './components/ProductSizes/ProductSizes.css'
 
 function App() {
     return (
-        <Container >
-            <Row className="text-center">
-                <Col md={{ span: 6, offset: 3 }}>
-                    <h6 className="mt-3">Main - 2</h6>
-                    <div>
-                        <ShoppingCartButton text="Add to Cart - $250" />
-                        <ShoppingCartButton text="Add to Cart - $250" disabled={true} />
-                        <ShoppingCartButton backgroundColor={"#005f73"} text="Add to Cart - $250" />
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <ProductSizes/>
     );
 }
 export default App;
---
+++ src/components/ProductSizes/ProductSizes.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {Button, Container} from "react-bootstrap";
+
+function ProductSizes() {
+	const size = [
+		{value: 1, label: "XS"},
+		{value: 2, label: "S"},
+		{value: 3, label: "M"},
+		{value: 4, label: "L"},
+		{value: 5, label: "XL"},
+		{value: 6, label: "XXL"},
+		{value: 7, label: "3XL"},
+	];
+
+	return (
+			<Container>
+				<ul>
+					{size.map((size) => (
+							<UlSize key={size.value} size={size}/>
+					))}
+				</ul>
+			</Container>
+	)
+}
+
+function UlSize(props) {
+	const size = props.size;
+	return (
+			<li className="hr">
+				<Button variant="outline-secondary" id="start" className="rounded-0">{size.label}</Button>
+			</li>
+	);
+}
+
+export default ProductSizes;
