8b3b6815c7b7ae40e24caac1b0ae15644076b29b
KireikouEgor <kireikou.js@gmail.com>
2022-03-03 13:23:54 +0300

    Update ShoppingCartButton.js

--- src/components/ShoppingCartButton/ShoppingCartButton.js
+++ src/components/ShoppingCartButton/ShoppingCartButton.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./ShoppingCartButton.css";
-import { Button } from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
 const ShoppingCartButton = (props) => {
-    const text = props.text;
-    const disabled = props.disabled;
-    const backgroundColor = props.backgroundColor;
+	const text = props.text;
+	const disabled = props.disabled;
+	const backgroundColor = props.backgroundColor;
 
-    return (
-        <Button id="start" variant="dark" className="rounded-0" style={{ backgroundColor: backgroundColor }} disabled={disabled}>
-            {text}
-        </Button>
-    );
+	return (
+			<div className="btnAdd">
+				<Button variant="dark" className="rounded-0" style={{backgroundColor: backgroundColor}}
+				        disabled={disabled}>{text}</Button>
+			</div>
+	);
 };
 
 export default ShoppingCartButton;
