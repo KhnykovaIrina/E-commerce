d0fa7ea89c3d093482ed43a6344df7454d6e6946
KireikouEgor <kireikou.js@gmail.com>
2022-03-05 19:18:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import BlueButton from "./components/BlueButton";
 import ProductSizes from "./components/ProductSizes";
 
 function App() {
-    return (
-        <Container >
-            <Row className="text-center">
-                <Col md={{ span: 6, offset: 3 }}>
-                    <h6 className="mt-3">Main - 5</h6>
-                    <div>
-                        <BlueButton text="Buy Now" />
-                        <ProductSizes/>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+	return (
+			<Container>
+				<BlueButton text="Buy Now"/>
+				<ProductSizes/>
+			</Container>
+	);
 }
+
 export default App;
 
