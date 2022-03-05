dd07ff14c4dd6d3900b82154f96524d023d3a6d9
KireikouEgor <kireikou.js@gmail.com>
2022-03-05 18:09:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 import OutlineButton from './components/OutlineButton';
 
 function App() {
-    return (
-        <Container >
-            <Row className="text-center">
-                <Col md={{ span: 4, offset: 4 }}>
-                    <div>
-                        <OutlineButton text="Shop All" />
-                        <OutlineButton text="Buy Now" />                       
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+	return (
+      <>
+        <OutlineButton text="Shop All"/>
+      </>
+	);
 }
+
 export default App;
 
