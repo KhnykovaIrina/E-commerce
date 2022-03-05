19ad002077f1042fca79748b2710cee62999e00a
KireikouEgor <kireikou.js@gmail.com>
2022-03-05 18:09:30 +0300

    Update OutlineButton.js

--- src/components/OutlineButton/OutlineButton.js
+++ src/components/OutlineButton/OutlineButton.js
@@ -1,13 +1,17 @@
-import React from "react";
-import "./OutlineButton.css";
-import {Button} from "react-bootstrap";
+import React from "react"
+import './scss/OutlineButton.scss'
+import {Button, Container} from "react-bootstrap";
 
 const OutlineButton = (props) => {
 	const text = props.text;
 	return (
-			<div className="btn">
-				<Button className="rounded-0" variant="outline-dark">{text}</Button>
-			</div>
+			<Container className="text-center">
+				<div className="btn">
+					<Button className="rounded-0" variant="outline-dark"
+					        style={{fontSize: 25}}
+					>{text}</Button>
+				</div>
+			</Container>
 	);
 };
 
